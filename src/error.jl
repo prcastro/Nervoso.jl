7ba5525c30155c360362c45eb35782318ce96e7a
prcastro <p.oliveira.castro@gmail.com>
2015-07-16 08:50:02 -0300

    Error bug

--- src/error.jl
+++ src/error.jl
@@ -3,10 +3,10 @@
 #============================#
 
 "Quadratic error between the output of the network and a example's output"
-quaderror(output, example) = 0.5*(norm(output - example)^2)
+quaderror(output, example) = 0.5*norm(output - example)^2
 
 "Derivative of the quadratic error with respect to the values of the last layer"
-quaderrorprime(last_layer, example, act::Function) = (act(last_layer) - example) * map(der(act), last_layer)
+quaderrorprime(last_layer, example, act::Function) = (act(last_layer) - example) .* map(der(act), last_layer)
 
 # Insert quaderrorprime into derivatives dictionary
 derivatives[quaderror] = quaderrorprime

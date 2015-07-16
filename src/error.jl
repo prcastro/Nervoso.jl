99c7945af75adb8b9e5e2d0ccc08d31b90764ef9
prcastro <p.oliveira.castro@gmail.com>
2015-07-16 12:43:29 -0300

    Revert change in quaderror

--- src/error.jl
+++ src/error.jl
@@ -3,7 +3,7 @@
 #============================#
 
 "Quadratic error between the output of the network and a example's output"
-quaderror(last_layer, example, act::Function) = 0.5*norm(example - act(last_layer))^2
+quaderror(output, example) = 0.5*norm(example - output)^2
 
 "Derivative of the quadratic error with respect to the values of the last layer"
 quaderrorprime(last_layer, example, act::Function) = (example - act(last_layer)) .* map(der(act), last_layer)

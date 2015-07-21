144a084d6da4a3a9534e771a7096e8fd8fcf184e
Paulo R. O. Castro <brk00@fake.server>
2015-07-21 17:15:01 -0300

    Start tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,36 @@
 using NeuralNetsLite
-using Base.Test
+using FactCheck
 
-# write your own tests here
-@test 1 == 1
+facts("Layer") do
+    context("Build a layer") do
+    end
+
+    context("Basic informations of a layer") do
+    end
+
+    context("Update and activate a layer") do
+    end
+end
+
+facts("Network") do
+    context("Build a network") do
+    end
+
+    context("Basic informations of a network") do
+    end
+
+    context("Propagating examples") do
+    end
+
+    context("Training") do
+    end
+
+    context("Assessing performance") do
+    end
+end
+
+facts("Activation functions") do
+end
+
+facts("Cost functions") do
+end

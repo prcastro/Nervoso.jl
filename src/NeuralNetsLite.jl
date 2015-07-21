cdc793c0c25b6a22c410b9458627c54584dcc2e7
Paulo R. O. Castro <brk00@fake.server>
2015-07-21 16:05:06 -0300

    Correct perfomance.jl path

--- src/NeuralNetsLite.jl
+++ src/NeuralNetsLite.jl
@@ -8,6 +8,6 @@ include("feedforward/layer.jl")
 include("activation.jl")
 include("cost.jl")
 include("feedforward/network.jl")
-include("performance.jl")
+include("feedforward/performance.jl")
 
 end # module NeuralNetsLite

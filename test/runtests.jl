dbbd16ba2fb24263009fc9a9727f30cbb0eb2c8a
prcastro <p.oliveira.castro@gmail.com>
2015-07-29 12:20:15 -0300

    New FactCheck syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,22 @@ using FactCheck
 
 facts("Layer") do
     context("Build a layer") do
-        @fact FFNNLayer(10) => anything
-        @fact FFNNLayer(10, softmax, bias = false) => anything
+        @fact FFNNLayer(10) --> anything
+        @fact FFNNLayer(10, softmax, bias = false) --> anything
 
         layer1 = FFNNLayer(10)
         layer2 = FFNNLayer(10, softmax, bias = false)
 
         context("With Bias") do
-            @fact layer1.bias => true
-            @fact length(layer1.neurons) => 11
-            @fact layer1.activation == tanh => true
+            @fact layer1.bias --> true
+            @fact length(layer1.neurons) --> 11
+            @fact layer1.activation == tanh --> true
         end
         context("Without Bias and different activation function") do
             layer2 = FFNNLayer(10, softmax, bias = false)
-            @fact layer2.bias => false
-            @fact length(layer2.neurons) => 10
-            @fact layer2.activation == softmax => true
+            @fact layer2.bias --> false
+            @fact length(layer2.neurons) --> 10
+            @fact layer2.activation == softmax --> true
         end
     end
 
@@ -27,21 +27,21 @@ facts("Layer") do
 
     context("Basic informations of a layer") do
         context("Size and length") do
-            @fact size(layer1)   => 10
-            @fact length(layer2) => 10
-            @fact size(layer1)   => 10
-            @fact length(layer2) => 10
+            @fact size(layer1)   --> 10
+            @fact length(layer2) --> 10
+            @fact size(layer1)   --> 10
+            @fact length(layer2) --> 10
         end
 
         context("Access neurons") do
-            @fact layer1[1] => 0
-            @fact layer2[1] => 0
-            @fact layer1[end] => 0
-            @fact layer2[end] => 0
+            @fact layer1[1] --> 0
+            @fact layer2[1] --> 0
+            @fact layer1[end] --> 0
+            @fact layer2[end] --> 0
         end
 
         context("Access bias") do
-            @fact layer1[0] => 1.0
+            @fact layer1[0] --> 1.0
             @fact_throws layer2[0]
         end
 
@@ -49,47 +49,47 @@ facts("Layer") do
             # Iterate on layer1
             c = 0
             for i in layer1
-                @fact i => 0.0
+                @fact i --> 0.0
                 c += 1
             end
-            @fact c => 10
+            @fact c --> 10
 
             # Iterate on layer1
             c = 0
             for i in layer2
-                @fact i => 0.0
+                @fact i --> 0.0
                 c += 1
             end
-            @fact c => 10
+            @fact c --> 10
         end
     end
 
     context("Update and activate a layer") do
         context("Activate") do
-            @fact activate(layer1) => vcat([1.0], zeros(10))
-            @fact activate(layer2) => ones(10)/10
+            @fact activate(layer1) --> vcat([1.0], zeros(10))
+            @fact activate(layer2) --> ones(10)/10
         end
 
         context("Update") do
             @fact_throws update(layer1, [1])
             update!(layer1, ones(10))
-            @fact layer1.neurons => ones(11)
+            @fact layer1.neurons --> ones(11)
 
             @fact_throws update(layer2, [1])
             update!(layer2, ones(10))
-            @fact layer2.neurons => ones(10)
+            @fact layer2.neurons --> ones(10)
         end
     end
 end
 
 facts("Network") do
     context("Build a network") do
-        @fact FFNNet(10,10,10) => anything
-        @fact FFNNet(10,10,10,10,10) => anything
+        @fact FFNNet(10,10,10) --> anything
+        @fact FFNNet(10,10,10,10,10) --> anything
 
         layer1 = FFNNLayer(10)
         layer2 = FFNNLayer(10, softmax, bias = false)
-        @fact FFNNet([layer1, layer2], 10) => anything
+        @fact FFNNet([layer1, layer2], 10) --> anything
 
         @fact_throws FFNNet(10)
         @fact_throws FFNNet(10,10)
@@ -99,13 +99,13 @@ facts("Network") do
     net2 = FFNNet(2,10,10,10,10)
 
     context("Basic informations of a network") do
-        @fact length(net1) => 2
-        @fact length(net2) => 4
+        @fact length(net1) --> 2
+        @fact length(net2) --> 4
     end
 
     context("Propagating examples") do
-        @fact (typeof(propagate(net1, ones(2))) == Vector{Float64}) => true
-        @fact (typeof(propagate(net2, ones(2))) == Vector{Float64}) => true
+        @fact (typeof(propagate(net1, ones(2))) == Vector{Float64}) --> true
+        @fact (typeof(propagate(net2, ones(2))) == Vector{Float64}) --> true
     end
 
     inputs = Vector{Float64}[
@@ -121,15 +121,15 @@ facts("Network") do
     ]
 
     context("Training") do
-        @fact train!(net1, inputs, outputs) => nothing
-        @fact train!(net2, inputs, outputs) => nothing
+        @fact train!(net1, inputs, outputs) --> nothing
+        @fact train!(net2, inputs, outputs) --> nothing
     end
 
     context("Assessing performance") do
-        @fact (typeof(meanerror(net1, inputs, outputs)) == Float64) => true
-        @fact (typeof(meanerror(net2, inputs, outputs)) == Float64) => true
-        @fact (typeof(classerror(net1, inputs, outputs)) == Int) => true
-        @fact (typeof(classerror(net2, inputs, outputs)) == Int) => true
+        @fact (typeof(meanerror(net1, inputs, outputs)) == Float64) --> true
+        @fact (typeof(meanerror(net2, inputs, outputs)) == Float64) --> true
+        @fact (typeof(classerror(net1, inputs, outputs)) == Int) --> true
+        @fact (typeof(classerror(net2, inputs, outputs)) == Int) --> true
     end
 end
 
@@ -138,42 +138,42 @@ facts("Activation functions") do
     layer2 = FFNNLayer(10, bias = false)
 
     context("tanh") do
-        @fact derivatives[tanh] == tanhprime => true
-        @fact der(tanh) == tanhprime => true
+        @fact derivatives[tanh] == tanhprime --> true
+        @fact der(tanh) == tanhprime --> true
 
-        @fact typeof(tanh(layer1)) == Vector{Float64} => true
-        @fact length(tanh(layer1)) => length(layer1) + 1
-        @fact tanh(layer1) => vcat([1.0], zeros(length(layer1)))
-        @fact typeof(der(tanh)(layer1)) == Matrix{Float64} => true
-        @fact  size(der(tanh)(layer1)) => (length(layer1) + 1, length(layer1) + 1)
+        @fact typeof(tanh(layer1)) == Vector{Float64} --> true
+        @fact length(tanh(layer1)) --> length(layer1) + 1
+        @fact tanh(layer1) --> vcat([1.0], zeros(length(layer1)))
+        @fact typeof(der(tanh)(layer1)) == Matrix{Float64} --> true
+        @fact  size(der(tanh)(layer1)) --> (length(layer1) + 1, length(layer1) + 1)
 
-        @fact typeof(tanh(layer1)) == Vector{Float64} => true
-        @fact length(softmax(layer2)) => length(layer2)
-        @fact tanh(layer2) => zeros(length(layer2))
-        @fact typeof(der(tanh)(layer2)) == Matrix{Float64} => true
-        @fact  size(der(tanh)(layer2)) => (length(layer2), length(layer2))
+        @fact typeof(tanh(layer1)) == Vector{Float64} --> true
+        @fact length(softmax(layer2)) --> length(layer2)
+        @fact tanh(layer2) --> zeros(length(layer2))
+        @fact typeof(der(tanh)(layer2)) == Matrix{Float64} --> true
+        @fact  size(der(tanh)(layer2)) --> (length(layer2), length(layer2))
     end
 
     context("softmax") do
-        @fact derivatives[softmax] == softmaxprime => true
-        @fact der(softmax) == softmaxprime => true
+        @fact derivatives[softmax] == softmaxprime --> true
+        @fact der(softmax) == softmaxprime --> true
 
-        @fact typeof(softmax(layer1)) == Vector{Float64} => true
-        @fact length(softmax(layer1)) => length(layer1) + 1
-        @fact typeof(der(softmax)(layer1)) == Matrix{Float64} => true
-        @fact  size(der(softmax)(layer1)) => (length(layer1) + 1, length(layer1) + 1)
+        @fact typeof(softmax(layer1)) == Vector{Float64} --> true
+        @fact length(softmax(layer1)) --> length(layer1) + 1
+        @fact typeof(der(softmax)(layer1)) == Matrix{Float64} --> true
+        @fact  size(der(softmax)(layer1)) --> (length(layer1) + 1, length(layer1) + 1)
 
-        @fact typeof(softmax(layer2)) == Vector{Float64} => true
-        @fact length(softmax(layer2)) => length(layer2)
-        @fact typeof(der(softmax)(layer2)) == Matrix{Float64} => true
-        @fact  size(der(softmax)(layer2)) => (length(layer2), length(layer2))
+        @fact typeof(softmax(layer2)) == Vector{Float64} --> true
+        @fact length(softmax(layer2)) --> length(layer2)
+        @fact typeof(der(softmax)(layer2)) == Matrix{Float64} --> true
+        @fact  size(der(softmax)(layer2)) --> (length(layer2), length(layer2))
     end
 
     context("logistic") do
-        @pending derivatives[σ] == σprime => true
-        @pending der(σ) == σprime => true
-        @pending derivatives[logistic] == logisticprime => true
-        @pending der(logistic) == logisticprime => true
+        @pending derivatives[σ] == σprime --> true
+        @pending der(σ) == σprime --> true
+        @pending derivatives[logistic] == logisticprime --> true
+        @pending der(logistic) == logisticprime --> true
     end
 end
 
